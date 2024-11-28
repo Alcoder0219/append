a744f20d54ed61bef46562544a9c45c236016008
Your Name <you@example.com>
2024-11-28 21:58:11 +0530

    28-Nov-2024

--- img/script.js
+++ img/script.js
@@ -13,3 +13,34 @@ let toggleBtn = () => {
 };
 
 toggleBtn();
+// toggle button end
+
+// Client Counter Start
+let view_counter = ()=>{
+    let counters = [
+        {id: "counter1", target: 232},
+        {id: "counter2", target: 521},
+        {id: "counter3", target: 1453},
+        {id: "counter4", target: 32},
+    ];
+    
+    let animationCounter = () => {
+        counters.forEach(counter => { 
+            let count = 0;
+            let duration = 2000;
+            const interval = Math.round(duration / counter.target);
+            const element = document.getElementById(counter.id);
+            const updateCounter = () => {
+                if (count < counter.target) {
+                    count++;
+                    element.textContent = count;
+                    setTimeout(updateCounter, interval);
+                }
+            };
+            updateCounter();
+        });
+    };
+    window.onload = animationCounter;
+}
+view_counter()
+// Client Counter End
